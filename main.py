dbb5581ed4c05bb7b8f8c28924042cb0308ad26f
Destiny Boyer <destiny@destinyboyer.com>
2017-04-25 19:40:10 -0700

    added initPherGrid. Tested

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ def initAntGrid(n, prob):
             else:
                 antGrid[i][j] = np.random.randint(1, 4)
     return antGrid
+
+def initPherGrid(n):
+    pherGrid = [[0.0 for i in range(n)] for j in range(n)]
+    pherGrid = numpy.array(pherGrid)
+    pherGrid[n/2,:,] = 1.00
+    pherGrid[:,0]= -0.01
+    pherGrid[n - 1:,] = -0.01
+    pherGrid[0,:]= -0.01
+    pherGrid[n - 1:,] = -0.01
+    pherGrid[:,n - 1] = -0.01
+    return pherGrid
